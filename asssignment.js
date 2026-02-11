b2560260e8e9a1acc81a45691b057350ab966039
Daniel- new <bassygit@gmail.com>
2026-02-11 02:04:40 +0100

    added a new line

--- asssignment.js
+++ asssignment.js
@@ -3,7 +3,7 @@ let fruits = ["apple", "banana", "orange", "mango"]
 for (let i = 0; i < fruits.length; i++) {
             console.log(fruits[i])
 }
-let a = 12
+let a = 13
 let b = 2
 let c = 3
 let d = 4
