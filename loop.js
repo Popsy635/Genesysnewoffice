d053a6e968cc3fb0499078054eb49ac1efa01991
popacool <afuluenupopsy@gmail.com>
2026-03-17 14:01:33 +0100

    code to loop through numbers 1 - 30 and find the largest of the multiples of 4 among them

--- loop.js
+++ loop.js
@@ -1,3 +1,12 @@
-for (let i = 1; i <= 30; i++) {
-    console.log(i);
+// looping through 30 and finding the largest of the multiples of 4
+let largestNo = 0
+
+for (let i = 1; i < 30; i++) {
+    if( i % 4 === 0 ){
+        if (i > largestNo){
+            largestNo = i
+        }
+    }
 }
+
+console.log(largestNo)
